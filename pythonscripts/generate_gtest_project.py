7ae771379e00b76d7c3d2fcc19faa22730693db1
liujie3 <liujie3@xianlai-inc.com>
2020-01-13 09:39:57 +0800

    12

--- pythonscripts/generate_gtest_project.py
+++ pythonscripts/generate_gtest_project.py
@@ -23,49 +23,30 @@ class GTestingConfig:
         self.gen_project_cmd = 'cmake .. -G "%s" -DCMAKE_BUILD_TYPE=%s' % (self.compiler, self.cfg_build_type)
         self.build_project_cmd = 'cmake --build . --config %s' % (self.cfg_build_type)
         self.project_name = os.path.relpath(".", "..")
-        self.gtest_include_dir = os.getenv("GTEST_INCLUDE_DIR")
-        self.gtest_lib_dir = os.getenv("GTEST_LIB_DIR")
+        self.gtest_include_dir = ""
+        self.gtest_lib_dir = ""
+        self.include_directories = ""
+        self.link_directories = ""
+        self.target_link_libraries = ""
+        self.source = ""
         self.file_type_filter_re = r'.*\.(h|hpp|c|cpp|cc|cxx)$'
 
     def generate_cmakelists(self):
         f = open(self.cfg_currentwork_dir + "/CMakeLists.txt", 'w', encoding="utf-8")
         f.write('cmake_minimum_required(VERSION 3.1)\n')
-        f.write('project(')
-        f.write(self.project_name)
-        f.write(')\n')
-        f.write('SET(CMAKE_DEBUG_POSTFIX "d" CACHE STRING "add a postfix, usually d on windows")\n')
-        f.write('SET(CMAKE_BUILD_TYPE "Debug" CACHE STRING "build type")\n')
+        f.write('project(%s)\n' % self.project_name)
         f.write('if (MSVC)\n')
         f.write('    SET(CMAKE_CXX_FLAGS_DEBUG "/Zi /Ob0 /Od /RTC1 /Od -DDEBUG  /MTd")\n')
-        f.write('    SET(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /MT")\n')
         f.write('else()\n')
         f.write('    SET(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O0 -g3 -DDEBUG ")\n')
-        f.write('    SET(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O3 -DNDEBUG")\n')
         f.write('endif()\n')
-        f.write('IF(${CMAKE_BUILD_TYPE} MATCHES "Debug")\n')
-        f.write('    SET(CMAKE_BUILD_POSTFIX "${CMAKE_DEBUG_POSTFIX}")\n')
-        f.write('ELSE()\n')
-        f.write('    SET(CMAKE_BUILD_POSTFIX "")\n')
-        f.write('ENDIF()\n')
-        f.write('SET(GTEST_INCLUDE_DIR ')
-        f.write(self.gtest_include_dir)
-        f.write(')\n')
-        f.write('SET(GTEST_LIB_DIR ')
-        f.write(self.gtest_lib_dir)
-        f.write(')\n')
         f.write('SET(THREADS_PREFER_PTHREAD_FLAG ON)\n')
         f.write('FIND_PACKAGE(Threads REQUIRED)\n')
-        f.write('INCLUDE_DIRECTORIES(${GTEST_INCLUDE_DIR})\n')
-        f.write('LINK_DIRECTORIES(${GTEST_LIB_DIR})\n')
-        f.write('SET(SRC_TEST ')
-        f.write(self.file_name_list(self.cfg_currentwork_dir))
-        f.write(')\n')
+        f.write('INCLUDE_DIRECTORIES(%s %s)\n' % (self.gtest_include_dir, self.include_directories))
+        f.write('LINK_DIRECTORIES(%s %s)\n' % (self.gtest_lib_dir, self.link_directories))
+        f.write('SET(SRC_TEST %s)\n' % (self.source))
         f.write('ADD_EXECUTABlE(${PROJECT_NAME}  ${SRC_TEST})\n')
-        f.write('target_link_libraries(${PROJECT_NAME} \n')
-        f.write('gtest${CMAKE_BUILD_POSTFIX}\n')
-        f.write('gtest_main${CMAKE_BUILD_POSTFIX}\n')
-        f.write('Threads::Threads\n')
-        f.write(')')
+        f.write('target_link_libraries(${PROJECT_NAME} %s)' % (self.target_link_libraries))
         f.close()
 
     def file_name_list(self, file_dir):
@@ -77,17 +58,26 @@ class GTestingConfig:
         return file_list
 
     def init(self, args):
-        if os.path.exists(cfg.gtest_include_dir) == False:
-            raise Exception("\33[91m\nGTEST_INCLUDE_DIR not exists: " +cfg. gtest_include_dir + "\nsetting environment variable [GTEST_INCLUDE_DIR] \033[0m")
-        if os.path.exists(cfg.gtest_lib_dir) == False:
-            raise Exception("\33[91m\nGTEST_LIB_DIR not exists: " + cfg.gtest_lib_dir + "\nsetting environment variable [GTEST_LIB_DIR] \033[0m")
-
         self.cfg_build_type = args.build_type
         self.cfg_currentwork_dir=args.path
         self.cfg_build_binary_dir = self. cfg_currentwork_dir+'/.build'
         self.project_name = os.path.relpath(self.cfg_currentwork_dir, self.cfg_currentwork_dir + "/..")
+        if cfg.load_dep_configfile() == False :
+            sys.exit()
+
+        if os.path.exists(cfg.gtest_include_dir) == False:
+            raise Exception("\33[91m\nGTEST_INCLUDE_DIR not exists: " +cfg. gtest_include_dir + "\033[0m")
+        if os.path.exists(cfg.gtest_lib_dir) == False:
+            raise Exception("\33[91m\nGTEST_LIB_DIR not exists: " + cfg.gtest_lib_dir + "\n\033[0m")
+
         self.generate_cmakelists()
         self.print_member_var()
+
+    def list2str(self, list_source):
+        str_source = ''
+        for item in list_source:
+            str_source += ( str(item) + ' ')
+        return str_source
 
     def print_member_var(self):
         print("\033[93m compiler             : \033[0m"+self.compiler)
@@ -98,6 +88,37 @@ class GTestingConfig:
         print("\033[93m project_name         : \033[0m"+self.project_name)
         print("\033[93m gtest_include_dir    : \033[0m"+self.gtest_include_dir)
         print("\033[93m gtest_lib_dir        : \033[0m"+self.gtest_lib_dir)
+    
+    def load_dep_configfile(self):
+        if os.path.isfile(self.cfg_currentwork_dir+"/dep.json") == False:
+            self.usage_dep_config()
+            return False
+        else:
+            import json
+            f = open(self.cfg_currentwork_dir+"/dep.json", encoding='utf-8')
+            js = json.load(f)
+            self.gtest_include_dir = js["gtest_include_dir"]
+            self.gtest_lib_dir = js["gtest_lib_dir"]
+            self.project_name = js["project_name"]
+            self.include_directories = self.list2str(js["include_directories"])
+            self.link_directories = self.list2str(js["link_directories"])
+            self.target_link_libraries = self.list2str(js["target_link_libraries"])
+            self.source = self.list2str(js["source"])
+            return True
+
+    def usage_dep_config(self):
+        print("\033[92m Please configure [ " + self.cfg_currentwork_dir + "/dep.json ], example: \033[0m\n")
+        print("\033[32m")
+        print("{")
+        print("    \"project_name\":\"googletest01\",")
+        print("    \"gtest_include_dir\":\"/usr/local/googletest/include\",")
+        print("    \"gtest_lib_dir\":\"/usr/local/googletest/lib\",")
+        print("    \"include_directories\":[\"/usr/local/include2\", \"/usr/local/include2\"],")
+        print("    \"link_directories\":[\"/usr/local/lib\",  \"/usr/local/lib64\"],")
+        print("    \"target_link_libraries\":[\"pthread\", \"boost_system\"],")
+        print("    \"source\":\"main.cpp\"")
+        print("}")
+        print("\033[0m")
 
 cfg = GTestingConfig()
 
